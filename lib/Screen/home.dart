6a2abc41edc5244a6866016dc0e27768eccb63e6
Tata <tatasamy1998@gmail.com>
2020-04-27 00:38:37 +0200

    add localization and firebase SDK

--- lib/Screen/home.dart
+++ lib/Screen/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:mo8tarib/localization.dart';
+import 'package:mo8tarib/main.dart';
+
 
 class home extends StatefulWidget{
 
@@ -9,13 +12,42 @@ class home extends StatefulWidget{
 }
 
 class _homeState extends State<home>{
+
+  var _swap =0;
+
+  void _switchlang(){
+
+    if(_swap==0){
+      MyApp.setLocale(context, Locale('en', 'UK'));
+      print("en");
+    _swap=1;
+    }
+    else {
+      MyApp.setLocale(context, Locale('ar', 'SA'));
+      print("ar");
+      _swap=0;
+    }
+  }
   @override
   Widget build(BuildContext context) {
-
+    var localization = AppLocalizations.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("nana"),
-      ) ,
+        title: Text(localization.translate('nana')),
+        //title: Text('tata'),
+      ),
+      body: Container(
+        child: Center(
+          child: FlatButton(
+            child: Text(localization.translate('tata')),
+            color: Theme.of(context).primaryColor,
+            onPressed: _switchlang,
+
+          ),
+
+        ),
+
+      ),
     );
 
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:mo8tarib/Screen/home.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:mo8tarib/localization.dart';
+
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  static void setLocale (BuildContext context ,Locale locale){
+  _MyAppState state = context.findAncestorStateOfType<_MyAppState>();
+  state.setLocale(locale);
+  }
+   @override
+    _MyAppState createState() =>_MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
+  Locale _locale=Locale('ar', 'SA');
+
+  void setLocale (Locale locale){
+
+    setState(() {
+      _locale=locale;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-   return MaterialApp (
+    return MaterialApp (
 
-     title: "Mo8tarib",
-     home: home(),
-
-   );
+      title: "Mo8tarib",
+      locale: _locale,
+      supportedLocales: [
+        Locale('ar', 'SA'),
+        Locale('en', 'UK'),
+      ],
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      localeResolutionCallback: (locale, supportedLocales) {
+        // Check if the current device locale is supported
+        for (var supportedLocale in supportedLocales) {
+          if (supportedLocale.languageCode == locale.languageCode &&
+              supportedLocale.countryCode == locale.countryCode) {
+            return supportedLocale;
+          }
+        }
+        return supportedLocales.first;
+      },
+      debugShowCheckedModeBanner: false,
+      home: home(),
+    );
   }
+
 }
+

c35bba667d6fb499b549f0827eaad8254645b359
Mohab Batta <mohab.batta@yahoo.com>
2020-05-14 01:51:01 -0700

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:mo8tarib/Screen/home.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:mo8tarib/Screen/login.dart';
 import 'package:mo8tarib/localization.dart';
 import 'package:mo8tarib/model/languageControler.dart';
-
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
-  static void setLocale (BuildContext context ,Locale locale){
-  _MyAppState state = context.findAncestorStateOfType<_MyAppState>();
-  state.setLocale(locale);
+  static void setLocale(BuildContext context, Locale locale) {
+    _MyAppState state = context.findAncestorStateOfType<_MyAppState>();
+    state.setLocale(locale);
   }
-   @override
-    _MyAppState createState() =>_MyAppState();
+
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
   Locale _locale;
 
-  void setLocale (Locale locale){
-
+  void setLocale(Locale locale) {
     setState(() {
-      _locale=locale;
+      _locale = locale;
     });
   }
+
   @override
   void didChangeDependencies() {
     languageControler controler = new languageControler();
-    controler.getLocale().then((locale){
-setState(() {
-  this._locale=locale;
-});});
+    controler.getLocale().then((locale) {
+      setState(() {
+        this._locale = locale;
+      });
+    });
     super.didChangeDependencies();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-   if(_locale==null){
-return Container(
-  child: Center(
-    child: CircularProgressIndicator(),
-  ),
-);
-   }else{
-     return MaterialApp (
-
-       title: "Mo8tarib",
-       locale: _locale,
-       supportedLocales: [
-         Locale('fr'),
-         Locale('ar', 'SA'),
-         Locale('en', 'UK'),
-       ],
-       localizationsDelegates: [
-         AppLocalizations.delegate,
-         GlobalMaterialLocalizations.delegate,
-         GlobalWidgetsLocalizations.delegate,
-         GlobalCupertinoLocalizations.delegate,
-       ],
-       localeResolutionCallback: (locale, supportedLocales) {
-         // Check if the current device locale is supported
-         for (var supportedLocale in supportedLocales) {
-           if (supportedLocale.languageCode == locale.languageCode &&
-               supportedLocale.countryCode == locale.countryCode) {
-             return supportedLocale;
-           }
-         }
-         return supportedLocales.first;
-       },
-       debugShowCheckedModeBanner: false,
-       home: home(),
-     );
-   }
+    if (_locale == null) {
+      return Container(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return MaterialApp(
+        title: "Mo8tarib",
+        locale: _locale,
+        supportedLocales: [
+          Locale('fr'),
+          Locale('ar', 'SA'),
+          Locale('en', 'UK'),
+        ],
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        localeResolutionCallback: (locale, supportedLocales) {
+          // Check if the current device locale is supported
+          for (var supportedLocale in supportedLocales) {
+            if (supportedLocale.languageCode == locale.languageCode &&
+                supportedLocale.countryCode == locale.countryCode) {
+              return supportedLocale;
+            }
+          }
+          return supportedLocales.first;
+        },
+        debugShowCheckedModeBanner: false,
+        home: Login(),
+      );
+    }
   }
-
 }
-

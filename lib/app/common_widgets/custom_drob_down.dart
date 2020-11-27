0ea36ea725a6d49a390e97dccef15dcb74f3dd18
Mohab Batta <mohab.batta@yahoo.com>
2020-11-27 02:14:33 +0200

    a

---
+++ lib/app/common_widgets/custom_drob_down.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:mo8tarib/global.dart';
+
+class CustomDropDown extends StatelessWidget {
+  final String nameText;
+  final String value;
+  final List<String> arrItems;
+  final Function getValue;
+  const CustomDropDown(
+      {Key key, this.nameText, this.value, this.arrItems, this.getValue})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Text(
+            nameText,
+            style: TextStyle(
+              fontSize: 22,
+              fontFamily: 'VarelaRound',
+            ),
+          ),
+        ),
+        DropdownButton<String>(
+          value: value,
+          icon: Icon(Icons.arrow_downward),
+          iconSize: 20,
+          elevation: 16,
+          style: TextStyle(
+              fontSize: 18, fontFamily: 'VarelaRound', color: Colors.black),
+          underline: Container(
+            height: 2,
+            color: foregroundColor,
+          ),
+          onChanged: getValue,
+          items: arrItems.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        )
+      ],
+    );
+  }
+}
